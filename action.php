709b7045c433d647b5b9f60a9deea8019b98f959
mubaig <mubaig10@yahoo.com>
2018-06-02 22:21:53 +0530

    Update action.php

--- action.php
+++ action.php
@@ -4,14 +4,14 @@ $fname=$_POST<'fname'>;
 $lname=$_POST<'lname'>;
 $country=$_POST<'country'>;
 
-$data=$fname.",".$lname","$country;
+$data=$fname.",".$lname",".$country;
 
 
 $file="file.csv";
 
 
 file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
-print "[h1 align=center]Thank you for submitting your email address![/h1]";
+print "<h1 align=center>Thank you for submitting your email address!</h1>";
 
 
 ?>
