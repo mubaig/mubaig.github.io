72a9c7613c877168b95aa8d9db867e39f3fac49c
mubaig <mubaig10@yahoo.com>
2018-06-02 22:06:53 +0530

    Update action.php

--- action.php
+++ action.php
@@ -1,9 +1,28 @@
 <?php
-// Get the name they entered in the form
-// We'll be naming the file this
-$file = $_POST['name'];
-// Get the email from the form
-$email = $_POST['email'];
-// We want the file to be a text file right?
-$ex = ".txt";
+
+$email=$_POST<'email'>;
+$name=$_POST<'name'>;
+$data=$name.",".$email;
+
+
+$file="file.csv";
+
+
+file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
+print "[h1 align=center]Thank you for submitting your email address![/h1]";
+
+
 ?>
+<html>
+<head>
+  <title>ThankYou Page</title>    
+</head>
+<body>
+  <h1><a href="form.html">GO BACK</a> </h1>    
+    
+</body>
+
+
+
+
+</html>
