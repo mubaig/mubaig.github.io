fbfa72b5d6b27173aaf10c4debe6bb1f99aa36d8
mubaig <mubaig10@yahoo.com>
2018-06-02 21:27:07 +0530

    Create action.php

---
+++ action.php
@@ -0,0 +1,9 @@
+<?php
+// Get the name they entered in the form
+// We'll be naming the file this
+$file = $_POST['name'];
+// Get the email from the form
+$email = $_POST['email'];
+// We want the file to be a text file right?
+$ex = ".txt";
+?>
